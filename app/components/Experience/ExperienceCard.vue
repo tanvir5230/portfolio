7e997bbbb4944e4f57eba1a7b037889ae3024911
tanvir5230 <tanvirbintouhid5230@gmail.com>
2025-07-25 14:56:43 +0600

    style: modified profile picture size and removed border from experience card on hover

--- app/components/Experience/ExperienceCard.vue
+++ app/components/Experience/ExperienceCard.vue
@@ -6,14 +6,14 @@ const { data } = defineProps<{ data: Experience }>()
 
 <template>
   <UCard variant="subtle" :ui="{ body: 'p-2 sm:px-6 sm:pt-2' }"
-    class="rounded-2xl transition-transform duration-700 hover:border-2 hover:border-primary hover:-translate-y-2">
+    class="rounded-2xl transition-transform duration-700 hover:-translate-y-2">
     <template #header>
       <h2 class="text-2xl font-bold text-left mb-4">{{ data.designation }}</h2>
-      <div class="flex items-center gap-4">
+      <div class="flex flex-wrap items-center gap-4">
         <ULink :to="data.company.url" target="_blank" class="text-left text-secondary hover:underline">
           {{ data.company.name }}
         </ULink>
-        <span class="text-gray-500"> | </span>
+        <span class="text-gray-500 hidden md:inline"> | </span>
         <span class="flex items-center gap-2 text-gray-500">
           <UIcon name="i-lucide-calendar" /> <span>{{ data.duration }}</span>
         </span>
